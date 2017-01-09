add4390d5639016d4bd706bd3e2201e4f7a0e5a2
Michael Bright <github.com@mjbright.net>
2017-01-09 15:56:11 +0000

    Created downloadURL to retry downloads
    Necessary as host resolution of play-with-docker seems really flaky

--- host_install.sh
+++ host_install.sh
@@ -1,4 +1,30 @@
 
-#curl -O https://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
-curl -O http://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
+URL=https://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
+FILE=kubectl
+
+downloadURL() {
+    URL="$1"; shift
+    FILE="$1"; shift
+
+    CMD="curl -O $URL"
+
+    LOOP=0
+
+    echo "$CMD > $FILE"
+    set -x
+    while ! $CMD > $FILE; do
+        let LOOP=LOOP+1
+        echo
+        echo "[$LOOP]: Trying again ..."
+        sleep 0.1
+    done
+    set +x
+}
+
+
+[ ! -f $FILE ] && {
+    downloadURL $URL $FILE
+    chmod +x kubectl
+    ls -altr kubectl 
+}
 
