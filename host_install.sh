ef2fa9166665445eb3562602491f8ee34b288980
Mike Bright <github.com@mjbright.net>
2017-01-09 16:32:13 +0100

    Added curl download of kubectl

---
+++ host_install.sh
@@ -0,0 +1,4 @@
+
+#curl -O https://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
+curl -O http://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
+
