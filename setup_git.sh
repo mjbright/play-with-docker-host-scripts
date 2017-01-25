ae887e96dbbb7378ca27e09ec61bde888975e538
mjbright <github.com@mjbright.net>
2017-01-25 15:20:56 +0100

    Typo ..

--- setup_git.sh
+++ setup_git.sh
@@ -1,6 +1,6 @@
 
 cp .gitconfig ~/.gitconfig
 
-# Once credentials are entered once they will stored for future use:
+# Once credentials are entered once they will be stored for future use:
 git config credential.helper store
 
