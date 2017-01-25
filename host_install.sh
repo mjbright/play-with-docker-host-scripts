e85109c157c94e8016e3557cc1c0248a3d9250be
mjbright <github.com@mjbright.net>
2017-01-25 15:40:43 +0100

    Added options to host_install.sh
    Now installs kubectl only if -k option given

--- host_install.sh
+++ host_install.sh
@@ -1,9 +1,14 @@
 
+INSTALL_KUBECTL=0
 KUBECTL_URL=https://storage.googleapis.com/kubernetes-release/release/v1.5.1/bin/linux/amd64/kubectl
 
+MACHINE_DRIVER_PWD=/usr/local/bin/docker-machine-driver-pwd 
 MACHINE_PWD_URL=https://github.com/franela/docker-machine-driver-pwd/releases/download/v0.0.2/docker-machine-driver-pwd.tar.gz
 
 #export PWD_SESSION_ID=dc75aca5_node1
+
+################################################################################
+# Functions:
 
 die() {
     echo "$0: die - $*" >&2
@@ -57,7 +62,7 @@ downloadMachineDriver() {
         tar zxf $FILE || exit 1
 
         # 386, not amd-64??!!
-        cp -a linux/386/docker-machine-driver-pwd  /usr/local/bin/docker-machine-driver-pwd 
+        cp -a linux/386/docker-machine-driver-pwd  $MACHINE_DRIVER_PWD
 
     }
 }
@@ -72,8 +77,11 @@ createMachine() {
     docker ps
 }
 
-downloadKubectl
-downloadMachineDriver
+################################################################################
+# MAIN:
+
+########################################
+# Check session id is set:
 
 [ -z "$PWD_SESSION_ID" ] && die "Set session id from browser url using session.rc,
     . session.rc URL
@@ -81,8 +89,25 @@ e.g.
     . session.rc http://play-with-docker.com/p/dc75aca5-3b20-46b2-98a0-973d00ef8797#dc75aca5_node1
 "
 
-
 echo "Using session id <<$PWD_SESSION_ID>>"
+
+########################################
+# Process cmd-line args if any:
+
+while [ ! -z "$1" ]; do
+    case $1 in
+        -k) INSTALL_KUBECTL=1
+     
+    esac
+done
+
+########################################
+# Download tools:
+
+[ $INSTALL_KUBECTL -eq 1 ] && downloadKubectl
+
+[ ! -f $MACHINE_DRIVER_PWD ] && downloadMachineDriver
+
 createMachine master1
 createMachine worker1
 
